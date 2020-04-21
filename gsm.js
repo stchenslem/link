d14a3f6486c558c9bcc515a2d6c14f2d9c37446b
Stchen Slem <stchen.sl@gmail.com>
2020-04-21 17:13:42 +0700

    Update gsm.js

--- gsm.js
+++ gsm.js
@@ -118,8 +118,8 @@ function auto_safeconvert() {
     var j = 0;
     var daftarPostingan = ["https://hosting.firmware88.com/p/what-is-big-data-technology.html",
       "https://frekuensi-terkuat.blogspot.com/2019/12/data-analytics-course.html",
-      "https://jumpertrick.blogspot.com/2015/03/big-data-solutions.html",
-      "https://webhosting.firmware88.com/p/what-is-big-data-technology.html",];
+      "https://webhosting.firmware88.com/p/what-is-big-data-technology.html",
+      "https://frekuensi-terkuat.blogspot.com/2019/09/learning-big-data.html",];
      var randomPostingan = daftarPostingan[Math.floor(Math.random()*daftarPostingan.length)];
      var a_to_vi = "";
      for (var i = 0; i < a_to_va; i++) {
