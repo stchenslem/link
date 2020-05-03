186d228e4d12d712c60de2d74fe448fe991b204a
Stchen Slem <stchen.sl@gmail.com>
2020-05-03 19:51:49 +0700

    Update 1tuserhp.js

--- 1tuserhp.js
+++ 1tuserhp.js
@@ -117,9 +117,7 @@ function auto_safeconvert() {
     var a_to_vh = false;
     var j = 0;
     var daftarPostingan = ["https://hosting.firmware88.com/p/what-is-big-data-technology.html",
-      "https://frekuensi-terkuat.blogspot.com/2019/12/data-analytics-course.html",
-      "https://webhosting.firmware88.com/p/what-is-big-data-technology.html",
-      "https://frekuensi-terkuat.blogspot.com/2019/09/learning-big-data.html",];
+     "https://webhosting.firmware88.com/p/what-is-big-data-technology.html",];
      var randomPostingan = daftarPostingan[Math.floor(Math.random()*daftarPostingan.length)];
      var a_to_vi = "";
      for (var i = 0; i < a_to_va; i++) {
