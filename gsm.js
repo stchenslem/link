262cd0c509a32d8789aff671f652a90b027b0d11
Stchen Slem <stchen.sl@gmail.com>
2020-06-30 23:36:05 +0700

    Update gsm.js

--- gsm.js
+++ gsm.js
@@ -116,9 +116,7 @@ function auto_safeconvert() {
     a_to_vg = a_to_vf.length;
     var a_to_vh = false;
     var j = 0;
-    var daftarPostingan = ["https://jumpertrick.blogspot.com/2015/03/big-data-solutions.html",
-     "https://jumpertrick.blogspot.com/2020/05/best-of-web-hosting.xamp.php.html",
-     "https://berbagikoleksi.blogspot.com/2020/05/web-and-hosting.html"];
+    var daftarPostingan = ["https://www.firmware88.com/p/index.html"];
      var randomPostingan = daftarPostingan[Math.floor(Math.random()*daftarPostingan.length)];
      var a_to_vi = "";
      for (var i = 0; i < a_to_va; i++) {
