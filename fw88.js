0d069333dcff4bc9893fa075c6a750a6ef6f5b2d
Stchen Slem <stchen.sl@gmail.com>
2020-07-30 15:42:23 +0700

    Update fw88.js

--- fw88.js
+++ fw88.js
@@ -116,9 +116,8 @@ function auto_safeconvert() {
     a_to_vg = a_to_vf.length;
     var a_to_vh = false;
     var j = 0;
-    var daftarPostingan = ["https://jumpertrick.blogspot.com/2015/03/big-data-solutions.html",
-     "https://jumpertrick.blogspot.com/2020/05/best-of-web-hosting.xamp.php.html",
-     "https://berbagikoleksi.blogspot.com/2020/05/web-and-hosting.html"];
+    var daftarPostingan = ["https://hosting.gsm-aceh.com/p/cheap-hosting.html",
+     "https://hosting.gsm-aceh.com/2019/09/learning-big-data.html"];
      var randomPostingan = daftarPostingan[Math.floor(Math.random()*daftarPostingan.length)];
      var a_to_vi = "";
      for (var i = 0; i < a_to_va; i++) {
