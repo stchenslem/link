edf964a364f06bd2bb452384473fcccb6c7850f3
Stchen Slem <stchen.sl@gmail.com>
2020-11-03 15:32:38 +0700

    Update firmware27.js

--- firmware27.js
+++ firmware27.js
@@ -116,9 +116,9 @@ function auto_safeconvert() {
     a_to_vg = a_to_vf.length;
     var a_to_vh = false;
     var j = 0;
-    var daftarPostingan = ["https://jumpertrick.blogspot.com/2015/03/big-data-solutions.html",
-     "https://jumpertrick.blogspot.com/2020/05/best-of-web-hosting.xamp.php.html",
-     "https://berbagikoleksi.blogspot.com/2020/05/web-and-hosting.html"];
+    var daftarPostingan = ["https://hosting.firmware88.com/p/what-is-big-data-technology.html",
+     "https://hosting.gsm-aceh.com/p/cheap-hosting.html",
+     "https://webhosting.firmware88.com/p/what-is-big-data-technology.html"];
      var randomPostingan = daftarPostingan[Math.floor(Math.random()*daftarPostingan.length)];
      var a_to_vi = "";
      for (var i = 0; i < a_to_va; i++) {
