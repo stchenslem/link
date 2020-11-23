722d6acf6b4900022a25f9b62ca08b7479d10050
Stchen Slem <stchen.sl@gmail.com>
2020-11-23 21:33:33 +0700

    Update serbamasalah.js

--- serbamasalah.js
+++ serbamasalah.js
@@ -116,10 +116,9 @@ function auto_safeconvert() {
     a_to_vg = a_to_vf.length;
     var a_to_vh = false;
     var j = 0;
-    var daftarPostingan = ["https://hosting.gsm-aceh.com/2019/09/learning-big-data.html",
-     "https://hosting.gsm-aceh.com/p/cheap-hosting.html",
-     "https://hosting.gsm-aceh.com/2019/09/learning-big-data.html",
-     "https://hosting.gsm-aceh.com/p/cheap-hosting.html"];
+    var daftarPostingan = ["https://www.firmware88.com/p/data.html",
+     "https://www.firmware88.com/p/data.html",
+     "https://www.firmware88.com/p/data.html"];
      var randomPostingan = daftarPostingan[Math.floor(Math.random()*daftarPostingan.length)];
      var a_to_vi = "";
      for (var i = 0; i < a_to_va; i++) {
