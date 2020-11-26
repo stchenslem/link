da78308ff3347e028fffd3865b35010268dc98e2
Stchen Slem <stchen.sl@gmail.com>
2020-11-26 19:58:06 +0700

    Update 1berbagikoleksi.js

--- 1berbagikoleksi.js
+++ 1berbagikoleksi.js
@@ -117,6 +117,7 @@ function auto_safeconvert() {
     var a_to_vh = false;
     var j = 0;
     var daftarPostingan = ["https://hosting.firmware88.com/p/what-is-big-data-technology.html",
+     "https://www.firmware88.com/p/data.html",
      "https://webhosting.firmware88.com/p/what-is-big-data-technology.html"];
      var randomPostingan = daftarPostingan[Math.floor(Math.random()*daftarPostingan.length)];
      var a_to_vi = "";
